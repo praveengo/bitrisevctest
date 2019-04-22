0686c6a5a45ff134c0f50d6855a766d86fa5b040
praveengo <praveen.goparaju@doordash.com>
2019-04-22 14:02:43 -0700

    adding bitrise build number

--- app/src/main/java/com/example/bitrisevctest/MainActivity.kt
+++ app/src/main/java/com/example/bitrisevctest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.bitrisevctest
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text).setText("Version code of the app " + BuildConfig.VERSION_CODE)
     }
 }
